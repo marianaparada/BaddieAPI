89563a48e567b8bd8c841606e5846a9884ca6383
Abigail Osinaga <a2024110495@estudiantes.upsa.bo>
2025-09-15 16:04:52 -0400

    arreglar AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,5 +6,6 @@ namespace BaddieAPI.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Micro> Micro { get; set; }
-    public DbSet<Helicoptero> Helicoptero => Set<Helicoptero>();
+    public DbSet<Helicoptero> Helicoptero { get; set; }
+    public DbSet<Avion> Aviones { get; set; }
 }
