482adfd8a7747c9bb1f2b64030f3836ffe7ef89a
Ana Camila Lopez Correa <a2024112021@estudiantes.upsa.edu.bo>
2025-09-15 15:54:22 -0400

    Tercer commit data helicoptero

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,4 +6,5 @@ namespace BaddieAPI.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Micro> Micro { get; set; }
+    public DbSet<Helicoptero> Helicoptero => Set<Helicoptero>();
 }
