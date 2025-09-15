e9ca988938c87813161256215b035f9030ca85a1
Nadia Martinez <a2024112081@estudiantes.upsa.edu.bo>
2025-09-15 16:07:22 -0400

    moto dbcontext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,4 +8,6 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
     public DbSet<Micro> Micro { get; set; }
     public DbSet<Helicoptero> Helicoptero { get; set; }
     public DbSet<Avion> Aviones { get; set; }
+    
+    public DbSet<Moto> Motos { get; set; }
 }
